97acdf445dd309c828947f8cddaec43a8ee8376a
38elements <mh19820223@gmail.com>
2017-08-31 00:59:14 +0900

    Add SetHeader()

--- request.go
+++ request.go
@@ -9,6 +9,9 @@ import (
 // DefaultTimeout is seconds of timeout.
 var DefaultTimeout = 15
 
+// DefaultContentType is a content-type of request.
+var DefaultContentType = "application/x-www-form-urlencoded"
+
 // Request wraps http.Request.
 type Request struct {
 	Req     *http.Request
@@ -30,6 +33,12 @@ func (r *Request) Send() (res *Response, err error) {
 	return
 }
 
+// SetHeader sets a value of request header.
+func (r *Request) SetHeader(key, value string) *Request {
+	r.Req.Header.Set(key, value)
+	return r
+}
+
 // NewRequest make a Request.
 func NewRequest(method, url string, body io.Reader, timeoutSecond int) (req *Request, err error) {
 	request, err := http.NewRequest(method, url, nil)
@@ -47,5 +56,12 @@ func NewRequest(method, url string, body io.Reader, timeoutSecond int) (req *Req
 // Get make a request whose method is GET.
 func Get(url string) (req *Request, err error) {
 	req, err = NewRequest("GET", url, nil, DefaultTimeout)
+	return
+}
+
+// Post make a request whose method is GET.
+func Post(url string) (req *Request, err error) {
+	req, err = NewRequest("Post", url, nil, DefaultTimeout)
+	req.SetHeader("Content-Type", DefaultContentType)
 	return
 }
