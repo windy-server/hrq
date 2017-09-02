093e5b623b11b0b01c7f04264cbb3541885904d1
38elements <38elements@users.noreply.github.com>
2017-09-02 21:19:20 +0900

    Add test for Get() and Post() (#6)

--- request_test.go
+++ request_test.go
@@ -1,6 +1,9 @@
 package hrq
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestHeader(t *testing.T) {
 	r, _ := Get("http://example.com")
@@ -13,5 +16,29 @@ func TestHeader(t *testing.T) {
 	v = r.GetHeader("foo")
 	if v != "" {
 		t.Errorf("DelHeader is wrong. v is %v", v)
+	}
+}
+
+func TestGet(t *testing.T) {
+	req, _ := Get("http://example.com")
+	if req.Method != "GET" {
+		t.Errorf("req.Method is wrong by Get(). req.Method is %v", req.Method)
+	}
+	if req.Timeout != time.Duration(DefaultTimeout)*time.Second {
+		t.Errorf("req.Timeout is wrong by Get(). req.Timeout is %v", req.Timeout)
+	}
+}
+
+func TestPost(t *testing.T) {
+	req, _ := Post("http://example.com", nil)
+	if req.Method != "POST" {
+		t.Errorf("req.Method is wrong by Post(). req.Method is %v", req.Method)
+	}
+	if req.Timeout != time.Duration(DefaultTimeout)*time.Second {
+		t.Errorf("req.Timeout is wrong by Post(). req.Timeout is %v", req.Timeout)
+	}
+	ct := req.GetHeader("Content-Type")
+	if ct != DefaultContentType {
+		t.Errorf("Content-Type is wrong by Post(). Content-Type is %v", ct)
 	}
 }
