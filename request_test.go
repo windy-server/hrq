5720fec54fb5a2c059f9465f3b45e7904256063b
38elements <38elements@users.noreply.github.com>
2017-09-11 22:00:31 +0900

    Add TestApplicationJSON() (#45)

--- request_test.go
+++ request_test.go
@@ -1,7 +1,9 @@
 package hrq
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,29 +15,29 @@ func TestGetRequest(t *testing.T) {
 		// test for header
 		h1 := r.Header.Get("h1")
 		if h1 != "h2" {
-			t.Errorf("h1 is wrong in TestGetRequest(). h1 is %v", h1)
+			t.Fatalf("h1 is wrong in TestGetRequest(). h1 is %v", h1)
 		}
 		h3 := r.Header.Get("h3")
 		if h3 != "h4" {
-			t.Errorf("h3 is wrong in TestGetRequest(). h3 is %v", h3)
+			t.Fatalf("h3 is wrong in TestGetRequest(). h3 is %v", h3)
 		}
 		// test for query
 		v1 := r.URL.Query().Get("abc")
 		if v1 != "def" {
-			t.Errorf("v1 is wrong in TestGetRequest(). v1 is %v", v1)
+			t.Fatalf("v1 is wrong in TestGetRequest(). v1 is %v", v1)
 		}
 		v2 := r.URL.Query().Get("hij")
 		if v2 != "klm" {
-			t.Errorf("v2 is wrong in TestGetRequest(). v2 is %v", v2)
+			t.Fatalf("v2 is wrong in TestGetRequest(). v2 is %v", v2)
 		}
 		// test for cookie
 		c1, _ := r.Cookie("c1")
 		if c1.Value != "v1" {
-			t.Errorf("c1 is wrong in TestGetRequest(). c1 is %v", c1)
+			t.Fatalf("c1 is wrong in TestGetRequest(). c1 is %v", c1)
 		}
 		c2, _ := r.Cookie("c2")
 		if c2.Value != "v2" {
-			t.Errorf("c2 is wrong in TestGetRequest(). c2 is %v", c2)
+			t.Fatalf("c2 is wrong in TestGetRequest(). c2 is %v", c2)
 		}
 		values := [][]string{
 			[]string{"a", "b"},
@@ -59,7 +61,7 @@ func TestGetRequest(t *testing.T) {
 	res, _ := req.Send()
 	text, _ := res.Text()
 	if text != "FooBar" {
-		t.Errorf("text is wrong in TestGetRequest(). text is %v", text)
+		t.Fatalf("text is wrong in TestGetRequest(). text is %v", text)
 	}
 	cookies := map[string]string{
 		"a": "b",
@@ -67,11 +69,11 @@ func TestGetRequest(t *testing.T) {
 	}
 	cm := res.CookiesMap()
 	if cookies["a"] != cm["a"] || cookies["b"] != cm["b"] {
-		t.Errorf("CookiesMap() is wrong in TestGetRequest(). cm is %v", cm)
+		t.Fatalf("CookiesMap() is wrong in TestGetRequest(). cm is %v", cm)
 	}
 	a := res.CookieValue("a")
 	if a != "b" {
-		t.Errorf("CookieValue() is wrong in TestGetRequest(). a is %v", a)
+		t.Fatalf("CookieValue() is wrong in TestGetRequest(). a is %v", a)
 	}
 }
 
@@ -80,19 +82,19 @@ func TestPostRequest(t *testing.T) {
 		r.ParseForm()
 		foo := r.PostForm["foo"][0]
 		if foo != "123" {
-			t.Errorf("foo is wrong in TestGetRequest(). foo is %v", foo)
+			t.Fatalf("foo is wrong in TestGetRequest(). foo is %v", foo)
 		}
 		bar := r.PostForm["bar"][0]
 		if bar != "&456" {
-			t.Errorf("bar is wrong in TestGetRequest(). bar is %v", bar)
+			t.Fatalf("bar is wrong in TestGetRequest(). bar is %v", bar)
 		}
 		c1, _ := r.Cookie("c1")
 		if c1.Value != "v1" {
-			t.Errorf("c1 is wrong in TestGetRequest(). c1 is %v", c1)
+			t.Fatalf("c1 is wrong in TestGetRequest(). c1 is %v", c1)
 		}
 		c2, _ := r.Cookie("c2")
 		if c2.Value != "v2" {
-			t.Errorf("c2 is wrong in TestGetRequest(). c2 is %v", c2)
+			t.Fatalf("c2 is wrong in TestGetRequest(). c2 is %v", c2)
 		}
 		values := [][]string{
 			[]string{"a", "b"},
@@ -119,7 +121,7 @@ func TestPostRequest(t *testing.T) {
 	res, _ := req.Send()
 	text, _ := res.Text()
 	if text != "FooBar" {
-		t.Errorf("text is wrong in TestGetRequest(). text is %v", text)
+		t.Fatalf("text is wrong in TestGetRequest(). text is %v", text)
 	}
 	cookies := map[string]string{
 		"a": "b",
@@ -127,11 +129,11 @@ func TestPostRequest(t *testing.T) {
 	}
 	cm := res.CookiesMap()
 	if cookies["a"] != cm["a"] || cookies["b"] != cm["b"] {
-		t.Errorf("CookiesMap() is wrong in TestGetRequest(). cm is %v", cm)
+		t.Fatalf("CookiesMap() is wrong in TestGetRequest(). cm is %v", cm)
 	}
 	a := res.CookieValue("a")
 	if a != "b" {
-		t.Errorf("CookieValue() is wrong in TestGetRequest(). a is %v", a)
+		t.Fatalf("CookieValue() is wrong in TestGetRequest(). a is %v", a)
 	}
 }
 
@@ -140,11 +142,11 @@ func TestMultipartFormData(t *testing.T) {
 		r.ParseMultipartForm(0)
 		foo := r.FormValue("foo")
 		if foo != "123" {
-			t.Errorf("foo is wrong in TestGetRequest(). foo is %v", foo)
+			t.Fatalf("foo is wrong in TestGetRequest(). foo is %v", foo)
 		}
 		bar := r.FormValue("bar")
 		if bar != "&456" {
-			t.Errorf("bar is wrong in TestGetRequest(). bar is %v", bar)
+			t.Fatalf("bar is wrong in TestGetRequest(). bar is %v", bar)
 		}
 	}))
 	url := server.URL
@@ -157,41 +159,67 @@ func TestMultipartFormData(t *testing.T) {
 	req.Send()
 }
 
+func TestApplicationJSON(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		var list []string
+		err = json.Unmarshal(body, &list)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if list[0] != "foo" || list[1] != "bar" {
+			t.Fatalf("Request data is wrong in TestJSON()")
+		}
+	}))
+	url := server.URL
+	data := []string{
+		"foo",
+		"bar",
+	}
+	req, _ := Post(url, data)
+	req.SetApplicationJSON()
+	req.Send()
+}
+
 func TestHeader(t *testing.T) {
 	r, _ := Get("http://example.com")
 	r.SetHeader("foo", "bar")
 	v := r.HeaderValue("foo")
 	if v != "bar" {
-		t.Errorf("SetHeader is wrong. v is %v", v)
+		t.Fatalf("SetHeader is wrong. v is %v", v)
 	}
 	r.DelHeader("foo")
 	v = r.HeaderValue("foo")
 	if v != "" {
-		t.Errorf("DelHeader is wrong. v is %v", v)
+		t.Fatalf("DelHeader is wrong. v is %v", v)
 	}
 }
 
 func TestGet(t *testing.T) {
 	req, _ := Get("http://example.com")
 	if req.Method != "GET" {
-		t.Errorf("req.Method is wrong by Get(). req.Method is %v", req.Method)
+		t.Fatalf("req.Method is wrong by Get(). req.Method is %v", req.Method)
 	}
 	if req.Timeout != time.Duration(DefaultTimeout)*time.Second {
-		t.Errorf("req.Timeout is wrong by Get(). req.Timeout is %v", req.Timeout)
+		t.Fatalf("req.Timeout is wrong by Get(). req.Timeout is %v", req.Timeout)
 	}
 }
 
 func TestPost(t *testing.T) {
 	req, _ := Post("http://example.com", nil)
 	if req.Method != "POST" {
-		t.Errorf("req.Method is wrong by Post(). req.Method is %v", req.Method)
+		t.Fatalf("req.Method is wrong by Post(). req.Method is %v", req.Method)
 	}
 	if req.Timeout != time.Duration(DefaultTimeout)*time.Second {
-		t.Errorf("req.Timeout is wrong by Post(). req.Timeout is %v", req.Timeout)
+		t.Fatalf("req.Timeout is wrong by Post(). req.Timeout is %v", req.Timeout)
 	}
 	ct := req.HeaderValue("Content-Type")
 	if ct != DefaultContentType {
-		t.Errorf("Content-Type is wrong by Post(). Content-Type is %v", ct)
+		t.Fatalf("Content-Type is wrong by Post(). Content-Type is %v", ct)
 	}
 }
 
@@ -200,6 +228,6 @@ func TestSetTimeout(t *testing.T) {
 	req.SetTimeout(100)
 	timeout := time.Duration(100) * time.Second
 	if req.Timeout != timeout {
-		t.Errorf("req.SetTimeout() is wrong.")
+		t.Fatalf("req.SetTimeout() is wrong.")
 	}
 }
